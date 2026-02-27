d401e0ac647491ee0762eeafefbdd324172f087a
unknown <poondlapriya426@gmail.com>
2026-02-27 21:46:13 +0530

    project fully working with correct output

--- app_ibm.py
+++ app_ibm.py
@@ -1,19 +1,30 @@
 from flask import Flask, request, render_template
-import pickle
 import numpy as np
+import pickle
 
 app = Flask(__name__)
-
-# --- WRITE THE LOADING CODE HERE ---
-# This loads your model into memory once when the app starts
-with open('payments.pkl', 'rb') as file:
-    model = pickle.load(file)
-# -----------------------------------
+model = pickle.load(open('payments.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# This is what you were missing !
-if __name__ =="__main__":
-    app.run(debug=True)
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get values from the form
+    input_features = [float(x) for x in request.form.values()]
+    features = [np.array(input_features)]
+    
+    # Make prediction
+    prediction = model.predict(features)
+    
+    # Determine the message
+    if prediction[0] == 1:
+        output = "Fraudulent"
+    else:
+        output = "Normal"
+        
+    return render_template('submit.html', prediction_text=f'Transaction is {output}')
+
+if __name__ == "__main__":
+    app.run ( debug=True )
